8293ba3c20168839a3459f197df1f18ec705da5b
Matt Lim <mlim@caltech.edu>
2017-06-19 20:20:26 -0700

    Add flipping functionality. Add initial blocks implementation.

--- screensavers.js
+++ screensavers.js
@@ -3,38 +3,68 @@
  * First pattern.
  * Draws gradient of lines. Lines are drawn line-by-line.
  */
-function lines() {
+function lines(flip) {
   if (ypos == 0) {
     setUpdateVals();
     currColor = randRgb();
   } else {
     currColor = updateRgb(currColor);
   }
+  var updates = [0, ypos, w, bh];
+  if (flip) {
+    flipUpdates(updates);
+  }
   ctx.fillStyle = rgb(currColor);
-  ctx.clearRect(0, ypos, w, bh);
-  ctx.fillRect(0, ypos, w, bh);
-  ypos = (ypos + bh) % h;
+  ctx.clearRect.apply(ctx, updates);
+  ctx.fillRect.apply(ctx, updates);
+  if (flip) {
+    ypos = (ypos + bw) % w; // pretend ypos is xpos
+  } else {
+    ypos = (ypos + bh) % h;
+  }
 }
 
 /*
  * Second pattern.
  * Draws gradient of lines. Lines are drawn block-by-block.
  */
-function lines2() {
+function lines2(flip, blocky=false) {
   if (ypos == 0 && xpos == 0) {
     setUpdateVals();
     currColor = randRgb();
-  } else if (xpos == 0) {
-    console.log("yo");
-    currColor = updateRgb(currColor);
+  } else {
+    if (blocky) {
+      currColor = updateRgb(currColor);
+    } else if (xpos == 0) {
+      currColor = updateRgb(currColor);
+    }
+  }
+  var updates = [xpos, ypos, bw, bh];
+  if (flip) {
+    flipUpdates(updates);
   }
   ctx.fillStyle = rgb(currColor);
-  ctx.clearRect(xpos, ypos, bw, bh);
-  ctx.fillRect(xpos, ypos, bw, bh);
-  xpos = (xpos + bw) % w;
-  if (xpos == 0) {
-    ypos = (ypos + bh) % h;
+  ctx.clearRect.apply(ctx, updates);
+  ctx.fillRect.apply(ctx, updates);
+  if (flip) {
+    xpos = (xpos + bh) % h; // pretend xpos is ypos
+    if (xpos == 0) {
+      ypos = (ypos + bw) % w; // pretend ypos is xpos
+    }
+  } else {
+    xpos = (xpos + bw) % w;
+    if (xpos == 0) {
+      ypos = (ypos + bh) % h;
+    }
   }
+}
+
+/*
+ * Third pattern.
+ * Draws blocks. Updates colors every block.
+ */
+function blocks(flip) {
+  lines2(flip, true);
 }
 
 
@@ -68,6 +98,7 @@ function setUpdateVals() {
   lo = 5;
   hi = 20;
   for (i = 0; i < N; i++) {
+    // updateVals[i] = randRange(lo, hi);
     updateVals[i] = randRange(lo, hi);
   }
 }
@@ -86,11 +117,49 @@ function boundRNum(n) {
 }
 
 
-/*** MISC ***/
+/*** INPUT FUNCTIONS ***/
+/*
+ * This function will be called when the body is clicked. Asks the user if they
+ * want to save a screenshot.
+ */
 function getUrl() {
-  var dataURL = canvas.toDataURL();
-  window.prompt("Image URL", dataURL);
+  if (window.confirm("Download screenshot?")) {
+    var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    window.location.href=image;
+  }
 }
+
+/*
+ * Flips rendering.
+ */
+function flipRendering() {
+  clearInterval(interval);
+  flipped = !flipped;
+  screenFunc(flipped);
+  interval = setInterval(screenFunc, updateTime, flipped);
+}
+
+
+/*** MISC ***/
+/*
+ * Flips an update vector.
+ * E.g.
+ * [x, y, width, height] -> [y, x, height, width]
+ */
+function flipUpdates(updates) {
+  swap(updates, 0, 1)
+  swap(updates, 2, 3)
+}
+
+/*
+ * Helper function to swap array elements. Does no error checking lol.
+ */
+function swap(arr, i, j) {
+  var tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
+
 
 /*
  * Initialize some global variables and choose which screensaver to run.
@@ -105,12 +174,21 @@ function start(i) {
   xpos = 0;
   ypos = 0;
   N = 3;
+  screenFunc = undefined;
+  flipped = false;
 
-  if (i == 0) {
-    lines();
-    setInterval(lines, 450);
-  } else if (i == 1) {
-    lines2();
-    setInterval(lines2, 50);
+  // Set key listener
+  window.onkeyup = function (e) {
+    var code = e.keyCode ? e.keyCode : e.which;
+    if (code == 70) { // f
+      flipRendering();
+    }
   }
+
+  funcs = [lines, lines2, blocks];
+  updateTimes = [450, 50, 50];
+  screenFunc = funcs[i];
+  updateTime = updateTimes[i];
+  screenFunc(flipped);
+  interval = setInterval(screenFunc, updateTime, flipped); // used in flipRendering()
 }
